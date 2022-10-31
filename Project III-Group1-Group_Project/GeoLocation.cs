b8bf4e40a237e7096ac01a8d180cc7a72b2b7399
talon-ernst <talonernst@rogers.com>
2022-10-31 13:26:43 -0400

    created GeoLocation class

---
+++ Project III-Group1-Group_Project/GeoLocation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_III_Group1_Group_Project
+{
+    internal class GeoLocation
+    {
+    }
+}
