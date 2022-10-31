af02b056d9e267777cf44b8c299d2b112a855587
Flavius Porumbiel <Fporumbiel8739@conestogac.on.ca>
2022-10-31 12:47:39 -0400

    created plane class

---
+++ Project III-Group1-Group_Project/Plane.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_III_Group1_Group_Project
+{
+    public class Plane
+    {
+        private string planeName { get; set; }
+        private string pilotFirstName { get; set; }
+        private string pilotLastName { get; set; }
+        private int planeCapacity { get; set; }
+        private string startingLocation { get; set; }
+        private string destination { get; set; }
+        private string departureTime { get; set; }
+        private string arrivalTime { get; set; }
+
+        public Plane(string planeName, string pilotFirstName, string pilotLastName, int planeCapacity, string startingLocation, string destination, string departureTime, string arrivalTime)
+        {
+            this.planeName = planeName;
+            this.pilotFirstName = pilotFirstName;
+            this.pilotLastName = pilotLastName;
+            this.planeCapacity = planeCapacity;
+            this.startingLocation = startingLocation;
+            this.destination = destination;
+            this.departureTime = departureTime;
+            this.arrivalTime = arrivalTime;
+        }
+        public override string ToString()
+        {
+            return planeName + " " + pilotFirstName + " " + pilotLastName + " " + planeCapacity + " " + startingLocation + " " + destination + " " + departureTime + " " + arrivalTime;
+        }
+
+
+    }
+}
--- Project III-Group1-Group_Project/PlaneModule.cs
+++ Project III-Group1-Group_Project/PlaneModule.cs
@@ -16,5 +16,15 @@ namespace Project_III_Group1_Group_Project
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
