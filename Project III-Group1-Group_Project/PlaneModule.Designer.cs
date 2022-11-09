d42b7f1ae96f4bbfe244dc477f584a584fa3c4e1
talon-ernst <talonernst@rogers.com>
2022-11-08 21:43:36 -0500

    Plane now turns in real time, 3 seconds for a 45 angle change and 6 for a 90, longitude and latitude also pause when it makes sense for them too

--- Project III-Group1-Group_Project/PlaneModule.Designer.cs
+++ Project III-Group1-Group_Project/PlaneModule.Designer.cs
@@ -42,11 +42,15 @@
             this.txtLatitude = new System.Windows.Forms.TextBox();
             this.txtLongitude = new System.Windows.Forms.TextBox();
             this.grpBoxCoordinateDetails = new System.Windows.Forms.GroupBox();
+            this.lblPlaneIsTurning = new System.Windows.Forms.Label();
+            this.lblCompassBearing = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.grpBoxPlaneTurning = new System.Windows.Forms.GroupBox();
+            this.btnRight90 = new System.Windows.Forms.Button();
+            this.btnLeft90 = new System.Windows.Forms.Button();
             this.btnLeft45 = new System.Windows.Forms.Button();
             this.btnRight45 = new System.Windows.Forms.Button();
-            this.btnLeft90 = new System.Windows.Forms.Button();
-            this.btnRight90 = new System.Windows.Forms.Button();
+            this.turningPlaneTimer = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.grpBoxCoordinateDetails.SuspendLayout();
@@ -192,17 +196,47 @@
             // 
             // grpBoxCoordinateDetails
             // 
+            this.grpBoxCoordinateDetails.Controls.Add(this.lblPlaneIsTurning);
+            this.grpBoxCoordinateDetails.Controls.Add(this.lblCompassBearing);
+            this.grpBoxCoordinateDetails.Controls.Add(this.label1);
             this.grpBoxCoordinateDetails.Controls.Add(this.txtLatitude);
             this.grpBoxCoordinateDetails.Controls.Add(this.txtLongitude);
             this.grpBoxCoordinateDetails.Controls.Add(this.lblLatitude);
             this.grpBoxCoordinateDetails.Controls.Add(this.lblLongitude);
             this.grpBoxCoordinateDetails.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.grpBoxCoordinateDetails.Location = new System.Drawing.Point(131, 3);
+            this.grpBoxCoordinateDetails.Location = new System.Drawing.Point(3, 78);
             this.grpBoxCoordinateDetails.Name = "grpBoxCoordinateDetails";
-            this.grpBoxCoordinateDetails.Size = new System.Drawing.Size(152, 90);
+            this.grpBoxCoordinateDetails.Size = new System.Drawing.Size(166, 143);
             this.grpBoxCoordinateDetails.TabIndex = 16;
             this.grpBoxCoordinateDetails.TabStop = false;
             this.grpBoxCoordinateDetails.Text = "Coordinate Details";
+            // 
+            // lblPlaneIsTurning
+            // 
+            this.lblPlaneIsTurning.AutoSize = true;
+            this.lblPlaneIsTurning.ForeColor = System.Drawing.Color.Red;
+            this.lblPlaneIsTurning.Location = new System.Drawing.Point(7, 120);
+            this.lblPlaneIsTurning.Name = "lblPlaneIsTurning";
+            this.lblPlaneIsTurning.Size = new System.Drawing.Size(0, 12);
+            this.lblPlaneIsTurning.TabIndex = 19;
+            // 
+            // lblCompassBearing
+            // 
+            this.lblCompassBearing.AutoSize = true;
+            this.lblCompassBearing.Location = new System.Drawing.Point(141, 85);
+            this.lblCompassBearing.Name = "lblCompassBearing";
+            this.lblCompassBearing.Size = new System.Drawing.Size(0, 12);
+            this.lblCompassBearing.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 12);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Current Compass Bearing: ";
             // 
             // grpBoxPlaneTurning
             // 
@@ -211,39 +245,12 @@
             this.grpBoxPlaneTurning.Controls.Add(this.btnLeft45);
             this.grpBoxPlaneTurning.Controls.Add(this.btnRight45);
             this.grpBoxPlaneTurning.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.grpBoxPlaneTurning.Location = new System.Drawing.Point(289, 3);
+            this.grpBoxPlaneTurning.Location = new System.Drawing.Point(3, 221);
             this.grpBoxPlaneTurning.Name = "grpBoxPlaneTurning";
             this.grpBoxPlaneTurning.Size = new System.Drawing.Size(208, 106);
             this.grpBoxPlaneTurning.TabIndex = 17;
             this.grpBoxPlaneTurning.TabStop = false;
             this.grpBoxPlaneTurning.Text = "Plane Turning Control Panel";
-            // 
-            // btnLeft45
-            // 
-            this.btnLeft45.Location = new System.Drawing.Point(106, 23);
-            this.btnLeft45.Name = "btnLeft45";
-            this.btnLeft45.Size = new System.Drawing.Size(91, 34);
-            this.btnLeft45.TabIndex = 18;
-            this.btnLeft45.Text = "Turn Left 45°";
-            this.btnLeft45.UseVisualStyleBackColor = true;
-            // 
-            // btnRight45
-            // 
-            this.btnRight45.Location = new System.Drawing.Point(10, 23);
-            this.btnRight45.Name = "btnRight45";
-            this.btnRight45.Size = new System.Drawing.Size(91, 34);
-            this.btnRight45.TabIndex = 19;
-            this.btnRight45.Text = "Turn Right 45°";
-            this.btnRight45.UseVisualStyleBackColor = true;
-            // 
-            // btnLeft90
-            // 
-            this.btnLeft90.Location = new System.Drawing.Point(10, 64);
-            this.btnLeft90.Name = "btnLeft90";
-            this.btnLeft90.Size = new System.Drawing.Size(91, 34);
-            this.btnLeft90.TabIndex = 20;
-            this.btnLeft90.Text = "Turn Left 90°";
-            this.btnLeft90.UseVisualStyleBackColor = true;
             // 
             // btnRight90
             // 
@@ -253,6 +260,41 @@
             this.btnRight90.TabIndex = 21;
             this.btnRight90.Text = "Turn Right 90°";
             this.btnRight90.UseVisualStyleBackColor = true;
+            this.btnRight90.Click += new System.EventHandler(this.btnRight90_Click);
+            // 
+            // btnLeft90
+            // 
+            this.btnLeft90.Location = new System.Drawing.Point(10, 64);
+            this.btnLeft90.Name = "btnLeft90";
+            this.btnLeft90.Size = new System.Drawing.Size(91, 34);
+            this.btnLeft90.TabIndex = 20;
+            this.btnLeft90.Text = "Turn Left 90°";
+            this.btnLeft90.UseVisualStyleBackColor = true;
+            this.btnLeft90.Click += new System.EventHandler(this.btnLeft90_Click);
+            // 
+            // btnLeft45
+            // 
+            this.btnLeft45.Location = new System.Drawing.Point(10, 24);
+            this.btnLeft45.Name = "btnLeft45";
+            this.btnLeft45.Size = new System.Drawing.Size(91, 34);
+            this.btnLeft45.TabIndex = 18;
+            this.btnLeft45.Text = "Turn Left 45°";
+            this.btnLeft45.UseVisualStyleBackColor = true;
+            this.btnLeft45.Click += new System.EventHandler(this.btnLeft45_Click);
+            // 
+            // btnRight45
+            // 
+            this.btnRight45.Location = new System.Drawing.Point(106, 24);
+            this.btnRight45.Name = "btnRight45";
+            this.btnRight45.Size = new System.Drawing.Size(91, 34);
+            this.btnRight45.TabIndex = 19;
+            this.btnRight45.Text = "Turn Right 45°";
+            this.btnRight45.UseVisualStyleBackColor = true;
+            this.btnRight45.Click += new System.EventHandler(this.btnRight45_Click);
+            // 
+            // turningPlaneTimer
+            // 
+            this.turningPlaneTimer.Tick += new System.EventHandler(this.turningPlaneTimer_Tick);
             // 
             // Form1
             // 
@@ -299,6 +341,10 @@
         private System.Windows.Forms.Button btnLeft90;
         private System.Windows.Forms.Button btnLeft45;
         private System.Windows.Forms.Button btnRight45;
+        private System.Windows.Forms.Label lblCompassBearing;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPlaneIsTurning;
+        private System.Windows.Forms.Timer turningPlaneTimer;
     }
 }
 
