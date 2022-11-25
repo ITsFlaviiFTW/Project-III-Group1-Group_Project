d9a177a93f0a942b445dfad33e74174c55fc907c
talon-ernst <talonernst@rogers.com>
2022-11-24 22:42:04 -0500

    added mstests for testing GeoLocation functions

---
+++ GeoLocationTestingProject/GeoLocationTests.cs
@@ -0,0 +1,11 @@
+namespace GeoLocationTestingProject
+{
+    [TestClass]
+    public class GeoLocationTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
